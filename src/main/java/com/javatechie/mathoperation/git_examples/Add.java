137769d398906facbf2eb7ce0c905a0520886c4c
sameera madhav <sameeramadhav@gmail.com>
2022-07-28 21:01:56 +0530

    Modified Add and Added Sub to Git Examples

--- src/main/java/com/javatechie/mathoperation/git_examples/Add.java
+++ src/main/java/com/javatechie/mathoperation/git_examples/Add.java
@@ -4,14 +4,16 @@ public class Add {
 
 	private int value1;
 	private int value2;
+	private int value3;
 
-	public Add(int value1, int value2) {
+	public Add(int value1, int value2,int value3) {
 		this.value1 = value1;
 		this.value2 = value2;
+		this.value3 = value3;
 
 	}
 
 	public int sum() {
-		return value1+value2;
+		return value1+value2+value3;
 	}
 }
---
+++ src/main/java/com/javatechie/mathoperation/git_examples/Sub.java
@@ -0,0 +1,18 @@
+package com.javatechie.mathoperation.git_examples;
+
+public class Sub {
+	
+	private int value1;
+	private int value2;
+
+	public Sub(int value1, int value2) {
+		this.value1 = value1;
+		this.value2 = value2;
+
+	}
+	
+	public int difference() {
+		return value2-value1;
+	}
+
+}
