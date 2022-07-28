13c20f2c53880528bc4d31eea0e2ed9fe6a8970d
sameera madhav <sameeramadhav@gmail.com>
2022-07-28 20:47:53 +0530

    Added Git Examples Math Operation to local Repo

---
+++ src/main/java/com/javatechie/mathoperation/git_examples/Add.java
@@ -0,0 +1,17 @@
+package com.javatechie.mathoperation.git_examples;
+
+public class Add {
+
+	private int value1;
+	private int value2;
+
+	public Add(int value1, int value2) {
+		this.value1 = value1;
+		this.value2 = value2;
+
+	}
+
+	public int sum() {
+		return value1+value2;
+	}
+}
---
+++ src/main/java/com/javatechie/mathoperation/git_examples/MathOperation.java
@@ -0,0 +1,10 @@
+package com.javatechie.mathoperation.git_examples;
+
+public class MathOperation {
+
+	public static void main(String args[]) {
+		Add add = new Add(5,5);
+		System.out.println(add.sum());
+	}
+	
+}
