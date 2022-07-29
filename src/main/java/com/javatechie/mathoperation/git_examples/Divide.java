ddd253a79fa2a3831b1cd852f75204033c8f8b6a
sameeramadhav <110183023+sameeramadhav@users.noreply.github.com>
2022-07-29 17:41:25 +0530

    Update Divide.java

--- src/main/java/com/javatechie/mathoperation/git_examples/Divide.java
+++ src/main/java/com/javatechie/mathoperation/git_examples/Divide.java
@@ -1,1 +1,15 @@
+public class Divide {
 
+	private int value1;
+	private int value2;
+
+	public Divide(int value1, int value2) {
+		this.value1 = value1;
+		this.value2 = value2;
+
+	}
+
+	public int quotient() {
+		return value2 % value1;
+	}
+}
