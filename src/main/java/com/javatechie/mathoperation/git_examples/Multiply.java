22fc915530e549837459a4b02fa89ac81f460196
sameera madhav <sameeramadhav@gmail.com>
2022-07-29 16:57:12 +0530

    Modified MathOperation and Added Multiply

---
+++ src/main/java/com/javatechie/mathoperation/git_examples/Multiply.java
@@ -0,0 +1,17 @@
+package com.javatechie.mathoperation.git_examples;
+
+public class Multiply {
+
+	private int value1;
+	private int value2;
+
+	public Multiply(int value1, int value2) {
+		this.value1 = value1;
+		this.value2 = value2;
+
+	}
+	
+	public int product() {
+		return value2*value1;
+	}
+}
